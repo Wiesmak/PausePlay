138e303639d5ad1cfbbd4f07ca209f209ee1b9f2
Gerard Gajda <gerard.gajda@gmail.com>
2024-01-25 23:04:09 +0100

    Dodaj pliki projektów.

---
+++ PausePlay.cpp
@@ -0,0 +1,24 @@
+﻿#include "framework.h"
+
+#define MAX_LOADSTRING 100
+
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPTSTR    lpCmdLine,
+    int       nCmdShow)
+{
+    INPUT inputs[2] = {};
+    ZeroMemory(inputs, sizeof(inputs));
+
+    inputs[0].type = INPUT_KEYBOARD;
+    inputs[0].ki.wVk = VK_MEDIA_PLAY_PAUSE;
+
+    inputs[1].type = INPUT_KEYBOARD;
+    inputs[1].ki.wVk = VK_MEDIA_PLAY_PAUSE;
+    inputs[1].ki.dwFlags = KEYEVENTF_KEYUP;
+
+    UINT uSent = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+
+    return uSent != ARRAYSIZE(inputs) ? 1 : 0;
+}
